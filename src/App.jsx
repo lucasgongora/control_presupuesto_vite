116b46ae2d01a2f86a4954345492475f29359726
lucasgongora <lucasdgongora@gmail.com>
2023-09-04 22:50:06 -0300

    corrección doble render
    durante la programacion y debbug de la aplicacion surgian impresiones en console.log de manera dublicadas.
    con este cambio se corrigió la doble renderizacion.

--- src/App.jsx
+++ src/App.jsx
@@ -8,64 +8,64 @@ import IconoNuevoGasto from './img/nuevo-gasto.svg'
 
 function App() {
 
-const [gastos, setGastos] = useState(
-  localStorage.getItem('gastos') ? JSON.parse(localStorage.getItem('gastos')) : []
-);
+  const [gastos, setGastos] = useState(
+    localStorage.getItem('gastos') ? JSON.parse(localStorage.getItem('gastos')) : []
+  );
 
-const[presupuesto, setPresupuesto] = useState(
-  Number(localStorage.getItem('presupuesto')) ?? 0
-);
-const[isValidPresupuesto, setIsValidPresupuesto] = useState(false);
+  const[presupuesto, setPresupuesto] = useState(
+    Number(localStorage.getItem('presupuesto')) ?? 0
+  );
+  const[isValidPresupuesto, setIsValidPresupuesto] = useState(false);
 
-const [modal, setModal] = useState(false);
-const [animarModal, setAnimarModal] = useState(false);
+  const [modal, setModal] = useState(false);
+  const [animarModal, setAnimarModal] = useState(false);
 
-const [gastoEditar, setGastoEditar] = useState({});
+  const [gastoEditar, setGastoEditar] = useState({});
 
-const [filtro, setFiltro] = useState('');
-const [gastosFiltrados, setGastosFiltrados] = useState([]);
+  const [filtro, setFiltro] = useState('');
+  const [gastosFiltrados, setGastosFiltrados] = useState([]);
 
-useEffect(() =>{
-  if (Object.keys(gastoEditar).length  > 0) {
+  useEffect(() =>{
+    if (Object.keys(gastoEditar).length  > 0) {
+      setModal(true);
+
+      setTimeout(() => {
+        setAnimarModal(true);
+      }, 500);
+    }
+  }, [ gastoEditar])
+
+  useEffect(() =>{
+    localStorage.setItem('presupuesto', presupuesto ?? 0)
+  },[presupuesto])
+
+  useEffect(() =>{
+    localStorage.setItem('gastos', JSON.stringify(gastos) ?? []); 
+  },[gastos])
+
+  useEffect(() =>{
+    if(filtro){
+      const gastosFiltrados = gastos.filter(gasto => gasto.categoria === filtro);
+      setGastosFiltrados(gastosFiltrados);
+    }
+  },[filtro]);
+
+  useEffect(() =>{
+    const presupuestoLS = Number(localStorage.getItem('presupuesto')) ?? 0;
+
+    if (presupuestoLS > 0) {
+      setIsValidPresupuesto(true)
+    }
+  },[]);
+
+  const handleNuevoGasto = () => {
     setModal(true);
+    setGastoEditar({});
 
     setTimeout(() => {
       setAnimarModal(true);
     }, 500);
   }
-}, [ gastoEditar])
-
-useEffect(() =>{
-  localStorage.setItem('presupuesto', presupuesto ?? 0)
-},[presupuesto])
-
-useEffect(() =>{
-  localStorage.setItem('gastos', JSON.stringify(gastos) ?? []); 
-},[gastos])
-
-useEffect(() =>{
-  if(filtro){
-    const gastosFiltrados = gastos.filter(gasto => gasto.categoria === filtro);
-    setGastosFiltrados(gastosFiltrados);
-  }
-},[filtro]);
-
-useEffect(() =>{
-  const presupuestoLS = Number(localStorage.getItem('presupuesto')) ?? 0;
-
-  if (presupuestoLS > 0) {
-    setIsValidPresupuesto(true)
-  }
-},[]);
-
-const handleNuevoGasto = () => {
-  setModal(true);
-  setGastoEditar({});
-
-  setTimeout(() => {
-    setAnimarModal(true);
-  }, 500);
-}
 
 const guardarGasto = gasto => {
   if (gasto.id) {
@@ -85,11 +85,11 @@ const guardarGasto = gasto => {
   }, 500);
 }
 
-const eliminarGasto = id => {
-  const gastosActualizados = gastos.filter( gasto => gasto.id !== id);
+  const eliminarGasto = id => {
+    const gastosActualizados = gastos.filter( gasto => gasto.id !== id);
 
-  setGastos(gastosActualizados)
-}
+    setGastos(gastosActualizados)
+  }
 
   return (
     <div className={modal ? 'fijar' : ''}>
